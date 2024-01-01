44c337ff018ab2353b42f4571b146e166ed58d94
Hizar62 <hizarsajjad632@gmail.com>
2024-01-02 03:37:09 +0500

    dashboard

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -1,4 +1,6 @@
+import 'package:expensetrackingapp/ui/pi_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -10,14 +12,51 @@ class DashBoard extends StatefulWidget {
 class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-        foregroundColor: Colors.white,
-      ),
-      body: Column(children: []),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Dashboard"),
+            centerTitle: true,
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+          ),
+          body:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Add Account",
+              style: TextStyle(fontSize: 20),
+            ),
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            Center(
+              child: Container(
+                height: 100,
+                width: 350,
+                color: Colors.black12,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Add Expense",
+              style: TextStyle(fontSize: 20),
+            ),
+            // const SizedBox(
+            //   height: 20,
+            // ),
+            Container(
+              height: 100,
+              width: 350,
+              color: Colors.black12,
+            ),
+            const MyPieChart()
+          ]),
+        ));
   }
 }
