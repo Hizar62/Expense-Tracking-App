e72d259904cb3f6739bf34a8be2792a8c76ac6b7
Hizar62 <hizarsajjad632@gmail.com>
2024-01-19 22:52:54 +0500

    add account

--- lib/ui/add_account.dart
+++ lib/ui/add_account.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
 
-class AddAccount extends StatelessWidget {
+class AddAccount extends StatefulWidget {
   const AddAccount({super.key});
 
   @override
+  State<AddAccount> createState() => _AddAccountState();
+}
+
+class _AddAccountState extends State<AddAccount> {
+  String? valueChoose;
+  List listItems = ["Bank Account", "Easy Paisa", "Jazz Cash", "Cerdit Card"];
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        // theme: ThemeData(primarySwatch: Colors.amber)
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            leading: BackButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            title: const Text("Add Account"),
-            centerTitle: true,
-            backgroundColor: Colors.black,
-            foregroundColor: Colors.white,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        title: const Text('Add Account'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: DropdownButton(
+            hint: const Text("Select Account Type"),
+            value: valueChoose,
+            onChanged: (newValue) {
+              setState(() {
+                valueChoose = newValue as String?;
+              });
+            },
+            items: listItems.map((valueItem) {
+              return DropdownMenuItem(
+                value: valueItem,
+                child: Text(valueItem),
+              );
+            }).toList(),
           ),
-          body: Column(
-            children: [TextFormField()],
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
